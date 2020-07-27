aead24b52effa30677bb040fbf4a7a3ca3c6f761
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-27 12:58:44 -0600

    Added js. Let's see if it connects to the api

---
+++ main.js
@@ -0,0 +1,72 @@
+//Baseball Project Code
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ffchar = document.getElementById('ffchar');
+
+const url = 'https://www.moogleapi.com/api/v1/characters/?results=10';
+
+let ffchars;
+
+fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+        ffchars = data.results;
+        console.log(ffchars)
+
+        ffchars.map(ffchar => {
+            console.log(ffchar.name.last)
+        })
+
+        let li = createNode('li'),
+            img = createNode('img'),
+            span = createNode('span');
+    })
+
+
+/*
+//Star Wars Addition (for reference ONLY)
+import { people } from './people.js'
+import { planets } from './planets.js'
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+function first(array, count = 10) {
+    return array.slice(0, count);
+}
+
+const peopleul = document.getElementById('person');
+const planetsul = document.getElementById('planet');
+
+first(people).map(person => {
+// people.slice(0, 9).map(person => {
+    let li = createNode('li'),
+        span = createNode('span');
+
+    span.innerText = `${person.name}`;
+
+    append(li, span);
+    append(peopleul, li);
+})
+
+first(planets).map(planet => {
+        let li = createNode('li'),
+            span = createNode('span');
+    
+        span.innerText = `${planet.name}`;
+    
+        append(li, span);
+        append(planetsul, li);
+    })
+*/
