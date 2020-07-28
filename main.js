9428c665307141922d8d42ff3837c03f7f42044d
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-28 11:04:49 -0600

    trying to limit number of cards

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-const url = 'https://www.moogleapi.com/api/v1/characters/?results=30';
+const url = 'https://www.moogleapi.com/api/v1/characters/?results=10';
 const ul = document.getElementById('ffchars');
 
 document.getElementById('ffchars').className = "card cardFlip";
