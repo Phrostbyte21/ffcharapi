b1db6600bc558d44c6faec93212ef36d075a4a9d
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-29 15:24:49 -0600

    Cleaning up code

--- main.js
+++ main.js
@@ -6,88 +6,38 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-function first(array, count = 25) {
+// The number of cards to draw on page load
+function first(array, count = 26) {
     return array.slice(0, count);
 }
 
+// Loops through the card draw. If there's no image, it skips 
+// that object in the array and goes to the next one.
 function addAuthor(author) {
-        if (!author.pictures[0]) {
-            index += 1;
-        } else {
-            let li = createNode('li'),
-            img = createNode('img'),
-            span = createNode('span');
-
-            img.src = author.pictures[0].url;
-            span.innerText = `${author.name}\n\nAge: ${author.age}\nGender: ${author.gender}\nRace: ${author.race}\nOrigin: ${author.origin}`;
-
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-
-            index += 1;
-        }       
-}
-
-const url = 'https://www.moogleapi.com/api/v1/characters/';
-const ul = document.getElementById('ffchars');
-let index = 0;
-
-document.getElementById('ffchars').className = "card cardFlip";
-
-let authors;
-
-fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-        authors = data;
-        console.log(authors)
-        
-        first(authors).map(addAuthor)
-
-    })
-
-document.getElementById("addCard").addEventListener("click", () => {
-    addAuthor(authors[index]);
-    document.getElementById("addCard").innerHTML = "Added!";
-    setTimeout(function(){ document.getElementById("addCard").innerHTML = "Add Another Card" }, 2000);
-});
-
-
-
-
-//Working Before if/then statement
-/*
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-function first(array, count = 25) {
-    return array.slice(0, count);
-}
-
-function addAuthor(author) {
-    let li = createNode('li'),
-            img = createNode('img'),
-            span = createNode('span');
+    if (!author.pictures[0]) {
+        index += 1;
+    } else {
+        let li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
 
         img.src = author.pictures[0].url;
-        span.innerText = `Name: ${author.name} \nGender: ${author.gender} \nJob: ${author.job} \nRace: ${author.race}`;
+        span.innerText = `${author.name}\n\nAge: ${author.age}\nGender: ${author.gender}\nRace: ${author.race}\nOrigin: ${author.origin}`;
 
         append(li, img);
         append(li, span);
         append(ul, li);
+
         index += 1;
+    }       
 }
 
+// Delaring variables
 const url = 'https://www.moogleapi.com/api/v1/characters/';
 const ul = document.getElementById('ffchars');
 let index = 0;
 
+// Displaying API data in HTML
 document.getElementById('ffchars').className = "card cardFlip";
 
 let authors;
@@ -102,9 +52,10 @@ fetch(url)
 
     })
 
+// Functionality for the button at the bottom of the page
+// for users to add another card.
 document.getElementById("addCard").addEventListener("click", () => {
     addAuthor(authors[index]);
     document.getElementById("addCard").innerHTML = "Added!";
     setTimeout(function(){ document.getElementById("addCard").innerHTML = "Add Another Card" }, 2000);
 });
-*/
